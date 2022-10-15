a829fe18557a3a8a669a7276f983524891ad65c6
{Mameta29} <{fc.kikkawa@gmail.com}>
2022-10-15 12:08:04 +0900

    try deploy

--- src/MainMint.js
+++ src/MainMint.js
@@ -4,18 +4,8 @@ import './App.css';
 import ShapeLogo from './assets/ShapeLogo.png';
 import NFT from './assets/CooperatorNFTs/pink_予備.png';
 import { Box, Flex, Image } from '@chakra-ui/react';
-// import { nft1 } from './assets/NFTmetadata/CooperatorNFT_1.json';
-// import { nft2 } from './assets/NFTmetadata/CooperatorNFT_2.json';
-// import { nft3 } from './assets/NFTmetadata/CooperatorNFT_3.json';
-// import { nft4 } from './assets/NFTmetadata/CooperatorNFT_4.json';
-// import { nft5 } from './assets/NFTmetadata/CooperatorNFT_5.json';
-// import { nft6 } from './assets/NFTmetadata/CooperatorNFT_6.json';
-// import { nft7 } from './assets/NFTmetadata/CooperatorNFT_7.json';
 
 const cooperatorNFTAddress = '0x744bb5FB0a184FEec3dCe93a0dDac40De19050A4';
-// const fs = require('fs');
-// const json1 = fs.readFileSync('./assets/NFTmetadata/CooperatorNFT_1.json');
-// const data1 = JSON.parse(json1);
 
 const MainMint = ({ accounts, setAccounts }) => {
   const mintAmount = 1;
@@ -31,14 +21,9 @@ const MainMint = ({ accounts, setAccounts }) => {
         signer
       );
       try {
-        // console.log('dataには〜〜〜', data1);
-        const response = await contract.mint(
-          BigNumber.from(mintAmount),
-          // 'ipfs://bafybeiaz75nmds4mv2ehmmcabdzvzej2pm5bxk4arnduzcbjp5szhig2e4/6.json',
-          {
-            value: ethers.utils.parseEther((0.03 * mintAmount).toString()),
-          }
-        );
+        const response = await contract.mint(BigNumber.from(mintAmount), {
+          value: ethers.utils.parseEther((0.03 * mintAmount).toString()),
+        });
         console.log('response: ', response);
       } catch (err) {
         console.log('error: ', err);
@@ -71,8 +56,3 @@ const MainMint = ({ accounts, setAccounts }) => {
 };
 
 export default MainMint;
-
-// contract 変えたのでデプロイする○
-// どうせデプロイするなら譲渡不可にしてから○
-// NFTのmintをできるように⇨７つのアカウントとjsonを紐づけてしまう？
-// それをするならmappng 画像表示する際にもそこからデータをもってこれないか
--- src/NavBar.js
+++ src/NavBar.js
@@ -6,14 +6,13 @@ import Twitter from './assets/social-media-icons/twitterLogo.png';
 const NavBar = ({ accounts, setAccounts }) => {
   const isConnected = Boolean(accounts[0]);
   const walletsOfCooperator = [
-    '0xAd9b9Ad87A4d1EE7D52382A0400D79B3714806cE',
-    '0x2184AF4Ffa9AF93e99C6AC56C4507B03dF8Eb938',
-    '0x70997970C51812dc3A010C7d01b50e0d17dc79C8',
-    '0x4bB9aCE65677028B15a0a1ab189e72b8817e2a11',
-    '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266',
-    '0xfD8600dF5DD9fe11688109f35398c3B09703D153',
-    '0xb4814B09A70F877F6200dc3e85078fAb972132D0',
-    '0xb5a05Af300C50baAadE18f8EA8AA7e856177Afe8',
+    '0xAd9b9Ad87A4d1EE7D52382A0400D79B3714806cE', // 1
+    '0x1D73928555663200d3CABEf15e53C0a857397B36', // 2 あっきーさん
+    '0xaB1839aD0a610aDBB60FF50843Ae860f4f25cdaC', // 3 ゆうちゃん
+    // '0x4bB9aCE65677028B15a0a1ab189e72b8817e2a11', // 4
+    // '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266', // 5
+    // '0xfD8600dF5DD9fe11688109f35398c3B09703D153', // 6
+    // '0xb4814B09A70F877F6200dc3e85078fAb972132D0', // 7
   ];
 
   async function connectAccount() {
